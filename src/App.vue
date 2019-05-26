ce8e8ab31b96c0fbf070086195be10f8f79ed64c
Anna Maria <annamaria92@inbox.ru>
2019-05-27 00:12:46 +0200

    Comment unused code

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,18 @@
         {{item}}
       </li>      
     </ul>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    // HelloWorld
   },
   computed: {
     audioTracks() {
