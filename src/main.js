b9a9412fe760ec2b46f94175372a7fb86b3cc4eb
Anna Maria <annamaria92@inbox.ru>
2019-05-29 14:47:09 +0200

    Add vuetify framework

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 import {store} from './store';
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  Vuetify,
   render: h => h(App),
 }).$mount('#app')
