de9b9a4eda2ba0199cad95b18a7817a5a0543092
Anna Maria <annamaria92@inbox.ru>
2019-05-30 22:19:10 +0200

    Reload audio component

--- src/components/AudioTrack.vue
+++ src/components/AudioTrack.vue
@@ -7,7 +7,7 @@
       Source is {{ isPlaying ? "playing" : "paused" }}: {{fileName}}
     </p>
     
-    <audio controls>
+    <audio ref="audioElem" controls>
       <source :src="fileName" type="audio/mpeg">
       Your browser does not support the audio element.
     </audio>
@@ -17,11 +17,39 @@
 <script>
 export default {
   name: 'AudioTrack',
+  data: function() {
+    return {
+      currentTrack: null,
+    };
+  },
   props: {
     singer: String,
     song: String,
     fileName: String,
     isPlaying: Boolean
+  },
+  methods: {
+    initAudio: function() {
+      if (this.currentTrack !== this.fileName) {
+        this.currentTrack = this.fileName;
+
+        // https://stackoverflow.com/a/8870315
+        this.$refs.audioElem.setAttribute('src', this.currentTrack); //change the source
+        this.$refs.audioElem.load(); //load the new source
+      }
+
+      if (this.isPlaying) {
+        this.$refs.audioElem.play();
+      } else {
+        this.$refs.audioElem.pause(); 
+      }
+    }
+  },
+  mounted() {
+    this.initAudio();
+  },
+  beforeUpdate() {
+    this.initAudio();
   }
 }
 </script>
