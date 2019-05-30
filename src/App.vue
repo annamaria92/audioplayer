c90cdf47dc3e0796109c993122b9122d7b53e9e3
Anna Maria <annamaria92@inbox.ru>
2019-05-30 22:35:26 +0200

    Add v-layout and v-flex only

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,52 @@
 <template>
   <v-app id="app">
-    <v-list>        
-      <v-list-tile
-        v-for="(item, index) in this.audioTracks"
-        :key="index"
-      >
-        <v-list-tile-content v-on:click="choose(index)">
-          <v-list-tile-title>
-            <span v-if="currentaudioIndex === index"> + </span>
-            {{ visibleIndex(index) }} : {{item.singer}} - {{item.song}}
-          </v-list-tile-title>
-        </v-list-tile-content>
-      </v-list-tile>
-    </v-list>
-    <div>
-      <AudioTrack
-        :singer="audioTracks[currentaudioIndex].singer"
-        :song="audioTracks[currentaudioIndex].song"
-        :fileName="musicServer + audioTracks[currentaudioIndex].fileName"
-        :isPlaying="this.isPlaying"
-      />
-    </div>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+          <v-list>        
+            <v-list-tile
+              v-for="(item, index) in this.audioTracks"
+              :key="index"
+            >
+              <v-list-tile-content v-on:click="choose(index)">
+                <v-list-tile-title>
+                  <span v-if="currentaudioIndex === index"> + </span>
+                  {{ visibleIndex(index) }} : {{item.singer}} - {{item.song}}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+          <div>
+            <AudioTrack
+              :singer="audioTracks[currentaudioIndex].singer"
+              :song="audioTracks[currentaudioIndex].song"
+              :fileName="musicServer + audioTracks[currentaudioIndex].fileName"
+              :isPlaying="this.isPlaying"
+            />
+          </div>
       
-    <div>
-      <!-- <v-btn
-        color="success"
-        v-if="!this.isPlaying"
-        v-on:click="play"
-      >
-        воспроизведение
-      </v-btn>
-      <v-btn
-        color="error"
-        v-if="this.isPlaying"
-        v-on:click="stop"
-      >
-        пауза
-      </v-btn> -->
-      <v-btn color="info" v-on:click="prev">←предыдущий</v-btn>
-      <v-btn color="info" v-on:click="next">следующий→</v-btn>
-    </div>
-    <!-- найти трек -->
+          <div>
+            <!-- <v-btn
+              color="success"
+              v-if="!this.isPlaying"
+              v-on:click="play"
+            >
+              воспроизведение
+            </v-btn>
+            <v-btn
+              color="error"
+              v-if="this.isPlaying"
+              v-on:click="stop"
+            >
+              пауза
+            </v-btn> -->
+            <v-btn color="info" v-on:click="prev">←предыдущий</v-btn>
+            <v-btn color="info" v-on:click="next">следующий→</v-btn>
+          </div>
+          <!-- найти трек -->
 
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+          <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+      </v-flex>
+    </v-layout>          
   </v-app>
 </template>
 
@@ -108,12 +112,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
