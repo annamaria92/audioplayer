41790e521ef2466773d3e446ffc2cfc999b74fb7
Anna Maria <annamaria92@inbox.ru>
2019-06-01 22:15:30 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+# deploy.sh
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo Linting..
+npm run lint
+echo Building. this may take a minute...
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'example.com' > CNAME
+
+echo Deploying..
+git init
+git add -A
+git commit -m 'deploy'
+
+# deploy
+git push -f git@github.com:<username>/<repo>.git <branch>
+
+cd -
