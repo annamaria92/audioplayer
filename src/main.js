f63d06440531ff73087a95149dfa0850126554a4
Anna Maria <annamaria92@inbox.ru>
2019-06-01 07:45:18 +0200

    Connect vuetify styles in a different way

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import App from './App.vue'
 import {store} from './store';
-import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify);
